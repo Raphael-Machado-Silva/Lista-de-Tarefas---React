21790ed4a004d9d9ea51c09ff29d91cf4093f2df
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-10-29 10:22:09 -0300

    update project

--- Lista Tarefas/src/App.jsx
+++ Lista Tarefas/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 import "./App.css"
 
 
@@ -25,30 +27,40 @@ function App() {
     },
   ]);
 
+  const addTodo = (text, category) => {
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 1000),
+      text,
+      category,
+      isCompleted: false,
+    }]
+
+    setTodos(newTodos)
+  }
+
+
+  const removeTodo = (id) => {
+
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter( //dando nulo ao id a ser excluido
+      todo => todo.id !== id ? todo : null
+    )
+    setTodos(filteredTodos)
+  }
+
+
   return (
     <div className='app'>
         <h1>Lista de Tarefas</h1>
         <div className="todo-list">
 
           {todos.map((todo) => (
-
-            <div className="todo" key={todo.id}>
-
-              <div className="content">
-                <p>{todo.text}</p>
-                <p className='category'>({todo.category})</p>
-              </div>
-
-              <div>
-                <button>Completar</button>
-                <button>x</button>
-              </div>
-
-            </div>
-
+            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
           ))}
 
         </div>
+        <TodoForm addTodo={addTodo}></TodoForm>
     </div>
   )
 }
